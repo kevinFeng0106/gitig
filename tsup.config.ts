e6c97e9241755cc15ddaa739de9b8406c88db053
lordmoon <1418015106@qq.com>
2025-07-06 16:09:31 +0800

    chore: setup bin of the cli

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
+  outDir: 'bin',
   splitting: false,
   sourcemap: false, // cli does not need sourcemaps
   clean: true,
