2b723bab47c567f2dc83a84d25ad5f81d6ca4be8
lordmoon <1418015106@qq.com>
2025-07-06 17:39:36 +0800

    feat: implement the first version of the gitig cli to create a .gitignore file in current working directory

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,51 @@
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+
 // src/index.ts
-console.log("hello gitig");
+var import_cac = __toESM(require("cac"));
+var import_node_fetch = __toESM(require("node-fetch"));
+var import_fs = __toESM(require("fs"));
+var cli = (0, import_cac.default)("gitig");
+var genTypeWhiteList = ["frontend"];
+var RAW_FILE_URL = "https://gitee.com/lord-moon/gitig-template/raw/master";
+cli.command("gen <type>", "Create a new .gitignore file with the specified type").action(async (type) => {
+  console.log("[gitig]: Generating .gitignore file with type " + type + " ...");
+  if (!genTypeWhiteList.includes(type)) {
+    throw new Error("[gitig]: Invalid type, please check the type list.");
+  }
+  ;
+  const targetRawFileUrl = `${RAW_FILE_URL}/${type}/general_gitignore.txt`;
+  console.log(`[gitig]: Downloading .gitignore file from ${targetRawFileUrl} ...`);
+  const reponse = await (0, import_node_fetch.default)(targetRawFileUrl, {
+    method: "GET",
+    headers: {
+      "Content-Type": "text/plain"
+    }
+  });
+  const text = await reponse.text();
+  const cwd = process.cwd();
+  console.log("[gitig]: Writing .gitignore file to " + cwd + " ...");
+  import_fs.default.writeFileSync(`${cwd}/.gitignore`, text);
+});
+cli.help();
+cli.parse();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log("hello gitig");
+import cac from "cac";
+import fetch from "node-fetch";
+import fs from "fs";
+
+const cli = cac("gitig");
+
+const genTypeWhiteList = ["frontend"];
+
+const RAW_FILE_URL = "https://gitee.com/lord-moon/gitig-template/raw/master";
+
+cli
+  .command("gen <type>", "Create a new .gitignore file with the specified type")
+  .action(async (type) => {
+    console.log("[gitig]: Generating .gitignore file with type " + type + " ...");
+
+    if(!genTypeWhiteList.includes(type)) {
+      throw new Error("[gitig]: Invalid type, please check the type list.");
+    };
+
+    const targetRawFileUrl = `${RAW_FILE_URL}/${type}/general_gitignore.txt`;
+    console.log(`[gitig]: Downloading .gitignore file from ${targetRawFileUrl} ...`);
+
+    const reponse = await fetch(targetRawFileUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+    const text = await reponse.text();
+
+    const cwd = process.cwd();
+    console.log("[gitig]: Writing .gitignore file to " + cwd + " ...");
+    fs.writeFileSync(`${cwd}/.gitignore`, text);
+  });
+
+cli.help();
+cli.parse();
